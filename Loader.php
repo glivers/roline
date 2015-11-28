dc26dcd26dc3b151b05197c672d619878e3e1c79
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-11-28 11:49:05 -0800

    Writing testing code for the Loader.php class

--- Loader.php
+++ Loader.php
@@ -1,5 +1,19 @@
-<?php
+<?php namespace Gliverich\Console;
+
+use Gliverich\Console\Commands\Database;
+use Symfony\Component\Console\Application;
+
+class Loader {
 
 
+	public function __construct(){
+
+		$console = new Application();
+		$console->add(new Database());
+		$console->run();
+
+	}
+
+}
 
 ?>
